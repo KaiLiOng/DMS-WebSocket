b76dcd3019a36b9ba87c6f7cad7c63a81bd456c7
Kai Li Ong <74152585+KaiLiOng@users.noreply.github.com>
2022-07-03 11:22:22 +0200

    Added stepper motor with hall sensor control

--- ESP32WebSocketServer/ESP32WebSocketServer.ino
+++ ESP32WebSocketServer/ESP32WebSocketServer.ino
@@ -15,6 +15,8 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <Stepper.h>
+
 
 //// Replace with your network credentials
 const char* ssid = "Vodafone-E3EA";
@@ -34,10 +36,22 @@ bool sixLED = 0;
 bool twelveLED = 0;
 bool offLED = 0;
 
-const int ledPinSet1 = 2;
-const int ledPinSet2 = 4;
-const int ledPinSet3 = 5;
+const int ledPinSet1 = 25;
+const int ledPinSet2 = 26;
+const int ledPinSet3 = 27;
 
+// Number of steps per output rotation
+const int stepsPerRevolution = 50;
+int stepCount = 0;
+
+const int HallSensor = 5;
+const int LED = 18;
+bool HomingDone = 0;
+
+bool Connected = 0;
+
+// Create Instance of Stepper library
+Stepper myStepper(stepsPerRevolution, 2, 0, 4, 16);
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -55,6 +69,7 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
+    String text_received = (char*)data;
     if (strcmp((char*)data, "toggle1") == 0) {
       threeLED = 1;
       ledSet1State = 1;
@@ -75,33 +90,13 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
       ledSet1State = 0;
       ledSet2State = 0;
       ledSet3State = 0;
+    }else if (text_received.indexOf("Stepper") == 0){
+      Serial.print("Stepper motor: ");
+      String StepperValue = text_received.substring(7);
+      Serial.println(StepperValue.toInt());
+      stepCount = 50 * StepperValue.toInt();
+      myStepper.step(stepCount);
     }
-
-//    if (threeLED == 1){
-//  
-//  Serial.println("Enter 3LED");
-//  }else if (sixLED == 1){
-////    ledSet1State = !ledSet1State;
-////    ledSet2State = !ledSet2State;
-////    ledSet3State = 0;
-//  digitalWrite(ledPinSet1, 1);
-//  digitalWrite(ledPinSet2, 1);
-//  digitalWrite(ledPinSet3, 0);
-//  Serial.println("Enter 6LED");
-//  }else if (twelveLED == 1){
-////    ledSet1State = !ledSet1State;
-////    ledSet2State = !ledSet2State;
-////    ledSet3State = !ledSet3State;
-//  digitalWrite(ledPinSet1, 1);
-//  digitalWrite(ledPinSet2, 1);
-//  digitalWrite(ledPinSet3, 1);
-//  Serial.println("Enter 12LED");
-//  }else if (offLED == 1){
-//  digitalWrite(ledPinSet1, LOW);
-//  digitalWrite(ledPinSet2, LOW);
-//  digitalWrite(ledPinSet3, LOW);
-//  Serial.println("Enter offLED");
-//  }
   }
 }
 
@@ -110,9 +105,12 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
   switch (type) {
     case WS_EVT_CONNECT:
       Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      HomingDone = 0;
+      Connected = 1;
       break;
     case WS_EVT_DISCONNECT:
       Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      Connected = 0;
       break;
     case WS_EVT_DATA:
       handleWebSocketMessage(arg, data, len);
@@ -152,6 +150,14 @@ void setup(){
   pinMode(ledPinSet3, OUTPUT);
   digitalWrite(ledPinSet3, LOW);
   
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
+
+  // set for Hall Sensor
+  pinMode(LED, OUTPUT);
+  pinMode(HallSensor, INPUT);
+  digitalWrite(LED,LOW);
+  
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -178,5 +184,12 @@ void loop() {
   digitalWrite(ledPinSet1, ledSet1State);
   digitalWrite(ledPinSet2, ledSet2State);
   digitalWrite(ledPinSet3, ledSet3State);
+  digitalWrite(LED, !digitalRead(HallSensor));
+  while(Connected == 1 && HomingDone == 0 && digitalRead(HallSensor) == 1){
+        myStepper.step(-100);
+        if(digitalRead(HallSensor) == 0){
+          HomingDone = 1;
+        }     
+   }   
 
 }
