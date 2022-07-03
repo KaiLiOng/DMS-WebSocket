3afa7dde84890938c10190c49458790da2842fee
Kai Li Ong <74152585+KaiLiOng@users.noreply.github.com>
2022-07-04 00:29:47 +0200

    Added OLED Display

--- ESP32WebSocketServer/ESP32WebSocketServer.ino
+++ ESP32WebSocketServer/ESP32WebSocketServer.ino
@@ -16,7 +16,13 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <Stepper.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 //// Replace with your network credentials
 const char* ssid = "Vodafone-E3EA";
@@ -52,6 +58,32 @@ bool Connected = 0;
 
 // Create Instance of Stepper library
 Stepper myStepper(stepsPerRevolution, 2, 0, 4, 16);
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -157,17 +189,45 @@ void setup(){
   pinMode(LED, OUTPUT);
   pinMode(HallSensor, INPUT);
   digitalWrite(LED,LOW);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
   
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
+    display.clearDisplay();
+    display.setTextSize(1);             // Normal 1:1 pixel scale
+    display.setTextColor(WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("Connecting to WiFi.."));
+    display.display();
   }
 
   // Print ESP Local IP Address
   Serial.println(WiFi.localIP());
-
+  display.clearDisplay();
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Connected!"));
+  display.println(F("IP Address:"));
+  display.println(WiFi.localIP());
+  display.display();
+  
   initWebSocket();
 
 //  // Route for root / web page
